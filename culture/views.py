e2277cc6435019381b025d2600e00e3407103ac8
wairimu.Kanene <mercykanene254@gmail.com>
2022-10-20 12:23:27 +0300

    added the filter photo by id functionality

--- culture/views.py
+++ culture/views.py
@@ -6,9 +6,16 @@ from . models import Category ,Image,Location
 def home(request):
   categories =  Category.objects.all()
   locations = Location.objects.all()
-  context = {'categories' :categories,  'locations':locations}
+  images = Image.objects.all()
+ 
+  
+  context = {'categories' :categories,  'locations':locations, 'images':images}
   return render(request, 'home.html' ,context)
+
 def viewPhoto(request,pk):
-  return render(request , 'galley.html')
+  images = Image.objects.get(id=pk)
+  
+  return render(request , 'galley.html' , {'images':images})
+
 def addPhoto(request):
   return render(request, 'add.html')
